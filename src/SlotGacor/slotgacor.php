bd09448514aa1e26e5e445cbfb23e71b81831e8f
borobodo <borobodohbiribidik@gmail.com>
2022-03-29 14:23:03 +0700

    Commit

---
+++ src/SlotGacor/slotgacor.php
@@ -0,0 +1,9 @@
+<?php
+namespace slotGacor;
+class PrintSomething
+{
+    public static function slotGacor()
+    {
+        return 'Daftar Link Bocoran Slot Gacor Hari Ini';
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'slotgacor/link-slot-gacor',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'slotgacor/link-slot-gacor' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
